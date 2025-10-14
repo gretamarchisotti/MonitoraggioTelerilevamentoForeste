445108c2421c4a4da0cfbc8351a849bd9d09cba6
gretamarchisotti <marchisottigreta@gmail.com>
2025-10-14 17:08:33 +0200

    Update LinearRegression.js

--- LinearRegression.js
+++ LinearRegression.js
@@ -45,6 +45,7 @@ Export.table.toAsset({
                     description: "trainingDataset", //Nome che gli do
                     assetId: "projects/planetunifi/assets/UNIBO/trainingDataset" //Percorso dell'asset (dato) che ho salvato
 })
+// Una volta che ho salvato i dati, posso commentare lo Step 1 e procedere solo con lo Step 2
 
 // STEP 2 *******************************************************************************************************
 
@@ -53,11 +54,12 @@ print(trainingDataset_loaded)
 
 // calculate m and q
 var fit = trainingDataset_loaded.reduceColumns({
-  reducer: ee.Reducer.linearFit(),
-  selectors: ['2018NDVI', 'Vapv_ha']
+                                                reducer: ee.Reducer.linearFit(), //Reducer che applico (regressione lineare)
+                                                selectors: ['2018NDVI', 'Vapv_ha'] //Colonne a cui applico il reducer
 });
 
-print(fit)
+print(fit) //Stampa il risultato
+//Offset: q; Scale: m
 
 var m = ee.Number(fit.get("scale"));
 var q = ee.Number(fit.get("offset"));
@@ -67,21 +69,44 @@ print(q)
 
 // make the prediction using the calculated regression coefficients
 var predictedGSVmap = medoidVariables.select("2018NDVI").multiply(m).add(q);
+// Applico il modello, sostituendo i valori di m e q e la x (NDVI) e y (volume)
+// All'inizio noi conoscevamo solo la biomassa e il volume di alcuni punti discreti
+// Integrando l'infromazione a terra con l'immagine satellitare, possiamo spazializzare, cioè estendere i dati a un'intera area di studio
 
 Map.addLayer(predictedGSVmap.clip(geometry), {min:0, max:500, palette: ["white", "yellow", "lightgreen", "green", "darkgreen"]})
+// min: 0 m3/ha e max: 500 m3/ha
 
+// L'NDVI da solo non consente di ottenere un dato accurato
+
+// Per vedere se il modello funziona, calcolo dei parametri di performance, come il Mean Square Error (MSE)
+// L'MSE ha il problema che è poco leggibile, perchè non è nelle stesse unità di misura della variabile, in quanto è al quadrato
+// Si calcola perciò il Root Mean Square Error, che corrisponde alla radice quadrata dell'MSE, che ha la stessa unità di misura della variabile
 // calculate RMSE and visualize
 var trainingDataset_loaded_withPredictions = trainingDataset_loaded.map(function(f){
-  var NDVI = ee.Number(f.get("2018NDVI"));
-  var Vapv_ha = ee.Number(f.get("Vapv_ha"));
-  var prediction = NDVI.multiply(m).add(q);
-  var scarto = Vapv_ha.subtract(prediction)
-  var scarto2 = scarto.pow(2)
-  return f.set("prediction", prediction).set("scarto", scarto).set("scarto2", scarto2)
+   var NDVI = ee.Number(f.get("2018NDVI"));
+   var Vapv_ha = ee.Number(f.get("Vapv_ha"));
+   var prediction = NDVI.multiply(m).add(q);
+   var scarto = Vapv_ha.subtract(prediction)
+   var scarto2 = scarto.pow(2) // 2 perchè è al quadrato
+   return f.set("prediction", prediction).set("scarto", scarto).set("scarto2", scarto2)
 })
-
 print(trainingDataset_loaded_withPredictions)
 
+// Calcolo la media degli scarti, che dovrebbe essere 0
+// Il valore è 0 perchè stiamo usando la regressione lineare, che tende a bilanciare gli scarti
+var mediaScarti = trainingDataset_loaded_withPredictions.aggregate_mean('scarto');
+print('mediaScarti:', mediaScarti.round());
+
+// Mean Square Error
+var mse = trainingDataset_loaded_withPredictions.aggregate_mean('scarto2');
+print('mse:', mse);
+
+// Root Mean Square Error: indica l'errore atteso, più leggibile perchè è nelle stesse unità di misura della variabile
+var rmse = ee.Number(mse).sqrt();
+print('RMSE:', rmse);
+// Ha un valore molto alto perchè l'NDVI da solo non basta
+
+// Creo dei grafici
 var chart_NDVIvsVapv = ui.Chart.feature.byFeature({
   features: trainingDataset_loaded_withPredictions,
   xProperty: '2018NDVI',
@@ -95,9 +120,7 @@ var chart_NDVIvsVapv = ui.Chart.feature.byFeature({
   pointSize: 6,
   colors: ['#1f77b4']
 });
-
-// Mostra il grafico
-print(chart_NDVIvsVapv);
+print(chart_NDVIvsVapv); // Mostra il grafico
 
 var chart = ui.Chart.feature.byFeature({
   features: trainingDataset_loaded_withPredictions,
@@ -112,9 +135,9 @@ var chart = ui.Chart.feature.byFeature({
   pointSize: 6,
   colors: ['#1f77b4']
 });
-
-// Mostra il grafico
-print(chart);
+print(chart); // Mostra il grafico
+// Vedo perrfettamente che l'NDVI da solo non basta
+// Riesce però a discriminare quei pixel a bassa presenza di biomassa
 
 // 
 var histogram = ui.Chart.feature.histogram({
@@ -130,11 +153,5 @@ var histogram = ui.Chart.feature.histogram({
 });
 
 print(histogram);
-
-var mediaScarti = trainingDataset_loaded_withPredictions.aggregate_mean('scarto');
-print('mediaScarti:', mediaScarti.round());
-
-var mse = trainingDataset_loaded_withPredictions.aggregate_mean('scarto2');
-print('mse:', mse);
-var rmse = ee.Number(mse).sqrt();
-print('RMSE:', rmse);
+// Volume misurato (y) e volume predetto (x)
+// Gli ultimi due grafici sono identici
